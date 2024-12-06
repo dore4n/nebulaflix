dfb44c0c5dc1dc5a26b5e680c0fbb344d4c64a6d
Lucas Emanuel Barboza Santos <lucasebsantos0@gmail.com>
2024-12-06 12:28:33 -0300

    feat: Adicionando components e pages

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+import Home from './pages/Home/Home'
+
+export default () => {
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
+}
---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div>
+        <img src='assets/nebulaflix-logo.svg' alt='logo'/>
+        <img src='' alt='avatar'/>
+    </div>
+  )
+}
+
+export default NavBar

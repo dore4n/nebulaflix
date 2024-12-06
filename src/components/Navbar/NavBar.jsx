039c60a4a61a6d1d7cfc0129efc62e3d59b74053
Lucas Emanuel Barboza Santos <lucasebsantos0@gmail.com>
2024-12-06 13:20:32 -0300

    feat: Primeira parte da navbar

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
-import './NavBar.css'
+import './Navbar.css'
 
-function NavBar() {
+
+import logo from '../../assets/nebulaflix-logo.svg'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
+
+function Navbar() {
   return (
-    <div>
-        <img src='assets/nebulaflix-logo.svg' alt='logo'/>
-        <img src='' alt='avatar'/>
+    <div className="navbar">
+        <div className="navbar-left">
+        <img src={logo} alt='logo'/>
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+        </ul>
+        </div>
+        <div className="navbar-right">
+          <img src={search_icon} alt='search' className='icons' />
+          <p>Children</p>
+          <img src={bell_icon} alt='bell' className='icons' />
+          <div className="navbar-profile">
+            <img src={profile_img} alt='bell' className='profile' />
+            <img src={caret_icon} alt='caret' />
+          </div>
+        </div>
     </div>
   )
 }
 
-export default NavBar
+export default Navbar
